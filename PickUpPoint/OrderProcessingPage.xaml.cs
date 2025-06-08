eb90af7166a8ae008344808a5b4d3a7d18db3cc1
SSAW13 <bogolyubovaw13@ya.ru>
2025-06-08 20:35:51 +0300

    WriteOff&Issue

--- PickUpPoint/OrderProcessingPage.xaml.cs
+++ PickUpPoint/OrderProcessingPage.xaml.cs
@@ -1,14 +1,63 @@
+using System.Data;
+
 namespace PickUpPoint;
 
 public partial class OrderProcessingPage : ContentPage
 {
+    DatabaseConnection _databaseConnection = new DatabaseConnection();
 	public OrderProcessingPage()
 	{
 		InitializeComponent();
 	}
-
-    private void SearchOrderClicked(object sender, EventArgs e)
+    DataTable dataTable;
+    private async void SearchOrderClicked(object sender, EventArgs e)
     {
+		if (!string.IsNullOrWhiteSpace(OrderIdEntry.Text))
+		{
+			string query = "select * from ordersOnWarehouse where id = " + OrderIdEntry.Text +";";
+            dataTable = _databaseConnection.ExecuteQuery(query);
 
+            InfoLabel.Text = "Заказ: ";
+            PhoneLabel.Text = "Номер клиента: ";
+            CellLabel.Text = "Ячейка: ";
+
+            try
+            {
+                Order.IsVisible = true;
+                InfoLabel.Text += "№" + dataTable.Rows[0][0] + " до " + dataTable.Rows[0][5];
+                PhoneLabel.Text += dataTable.Rows[0][4];
+                CellLabel.Text += dataTable.Rows[0][1];
+
+            }
+            catch (Exception)
+            {
+                Order.IsVisible= false;
+                await DisplayAlert("Ошибка", "Заказ не найден", "OK");
+
+            }
+        }
+    }
+
+    private async void IssueOrder(object sender, EventArgs e)
+    {
+        string query = "select IssueOrder(" + dataTable.Rows[0][0] + ", " + dataTable.Rows[0][1] + ")";
+        DataTable dataTable1 = _databaseConnection.ExecuteQuery(query);
+        await DisplayAlert("Успешно", "Заказ выдан", "ОК");
+        Order.IsVisible = false;
+    }
+
+    private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+	{
+        Entry entry = (Entry)sender;
+        string newText = e.NewTextValue;
+
+        if (string.IsNullOrWhiteSpace(newText)) return;
+
+        string filteredText = new string(newText.Where(char.IsDigit).ToArray());
+
+        if (newText != filteredText)
+        {
+            entry.Text = filteredText;
+        }
     }
 }
--- PickUpPoint/WriteOffPage.xaml.cs
+++ PickUpPoint/WriteOffPage.xaml.cs
@@ -1,9 +1,62 @@
+using System.Data;
+
 namespace PickUpPoint;
 
 public partial class WriteOffPage : ContentPage
 {
-	public WriteOffPage()
+    DatabaseConnection _databaseConnection = new DatabaseConnection();
+    public WriteOffPage()
 	{
 		InitializeComponent();
 	}
+    DataTable dataTable;
+    private async void SearchOrderClicked(object sender, EventArgs e)
+    {
+        if (!string.IsNullOrWhiteSpace(OrderIdEntry.Text))
+        {
+            string query = "select * from ordersOnWarehouseForWriteOff where id = " + OrderIdEntry.Text + ";";
+            dataTable = _databaseConnection.ExecuteQuery(query);
+
+            InfoLabel.Text = "Заказ: ";
+            PhoneLabel.Text = "Номер клиента: ";
+            CellLabel.Text = "Ячейка: ";
+
+            try
+            {
+                Order.IsVisible = true;
+                InfoLabel.Text += "№" + dataTable.Rows[0][0] + " до " + dataTable.Rows[0][5];
+                PhoneLabel.Text += dataTable.Rows[0][4];
+                CellLabel.Text += dataTable.Rows[0][1];
+
+            }
+            catch (Exception)
+            {
+                Order.IsVisible = false;
+                await DisplayAlert("Ошибка", "Заказ не найден", "OK");
+
+            }
+        }
+    }
+    private async void WriteOffOrder(object sender, EventArgs e)
+    {
+        string query = "select WriteOffOrder(" + dataTable.Rows[0][0] + ", " + dataTable.Rows[0][1] + ")";
+        DataTable dataTable1 = _databaseConnection.ExecuteQuery(query);
+        await DisplayAlert("Успешно", "Заказ списан", "ОК");
+        Order.IsVisible = false;
+    }
+
+    private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+    {
+        Entry entry = (Entry)sender;
+        string newText = e.NewTextValue;
+
+        if (string.IsNullOrWhiteSpace(newText)) return;
+
+        string filteredText = new string(newText.Where(char.IsDigit).ToArray());
+
+        if (newText != filteredText)
+        {
+            entry.Text = filteredText;
+        }
+    }
 }
